af37b7eac1f3544cd71409fddb4e817b02095754
Popikadir <62880018+Popikadir@users.noreply.github.com>
2020-04-09 21:32:00 +0200

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,111 @@
+def twitter_array 
+    ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end 
+def perform
+    data = twitter_array
+    case menu
+        when 1
+            nbr_joutnalist(data)
+            perform
+            return
+        when 2
+            nbr_handle(data)
+            perform
+            return
+        when 3
+            nbr_handle_aude(data)
+            perform
+            return
+        when 4
+            nbr_maj(data)
+            perform
+            return
+        when 5
+            nbr_onemaj(data)
+            perform
+            return
+        when 6
+            under(data)
+            perform
+            return
+        when 7
+            alpha_order(data)
+            perform
+            return
+        when 8
+            short_handle(data)
+            perform
+            return
+        when 9
+            position(data)
+            perform
+            return
+        end
+    
+end
+
+
+def menu
+    puts "Choisissez une action entre 1 et 9"
+    puts " "
+    puts "1. Combien y a-t-il de journalistes dans ce array ?"
+    puts " "
+    puts "2. Combien d'entrées contiennent un numéro ?"
+    puts " "
+    puts "3. Combien d'entrées contiennent les 4 lettres du prénom Aude à la suite (sans prendre en compte les majuscules) ?"
+    puts " "
+    puts "4. Combien commencent par une majuscule (première lettre juste après le @) ?"
+    puts " "
+    puts "5. Combien contiennent au moins une majuscule ?"
+    puts " "
+    puts "6. Combien y a-t-il de underscore _ dans tous les pseudos confondus ?"
+    puts " "
+    puts "7. Trie la liste d'entrées par ordre alphabétique."
+    puts " "
+    puts "8. Quels sont les 50 entrées les plus courts de ce array ?"
+    puts " "
+    puts "9. Quelle est la position dans le tableau de la personne @epenser ?"
+    puts " "
+    print ">"
+    user_input = Integer(gets)
+    end
+
+def nbr_joutnalist (data)#1
+	puts "Le tableau contient #{data.size} journalistes"
+    gets
+end
+def nbr_handle (data)#2
+	puts "Le tableau contient #{data.count{|handle|handle.count("0-9")>0}} handles avec des numéros"
+    gets
+end
+def nbr_handle_aude (data)#3 
+	puts "Le tableau contient #{data.count{|handle|handle.downcase.include?"aude"}} handles avec le prénom aude"
+	gets
+end
+def nbr_maj (data)#4 
+	puts "Le tableau contient #{data.count{|handle|/[[:upper:]]/.match(handle[1])}} handles qui commencent par une majuscule"
+	gets
+end
+def nbr_onemaj (data)#5 
+	puts "#{data.count {|handle|/[[:upper:]]/.match(handle)}} handles contiennent une majuscule"
+	gets
+end
+def under (data)#6 
+	puts "#{data.count{|handle|/[[_]]/.match(handle)}} handles contiennent un underscore"
+	gets
+end
+def alpha_order (data)#7 
+	puts "Voici le tableau avec les handles triées par ordre alphabétique"
+	puts data.sort #test
+	gets
+end
+def short_handle (data)#8 
+	puts "Voici les 50 handles les plus courts du tableau"
+	puts data.sort_by(&:length)[0..49]
+	gets
+end
+def position (data)#9
+	puts "Le handle @epenser est à l'index : #{data.index("@epenser")}"
+    gets
+end
+perform
